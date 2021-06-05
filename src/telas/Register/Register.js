435a392e5013cedd0a2d426e4efeb4b9a4324269
rodrigogeraldes <rodrigo.geraldes@aluno.ifsp.edu.br>
2021-06-05 13:29:50 -0300

    Estrutração Login - Alterações de Logo

---
+++ src/telas/Register/Register.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { KeyboardAvoidingView } from 'react-native';
+
+
+export default function Register(){
+    return(
+        <KeyboardAvoidingView>
+        </KeyboardAvoidingView>
+    );
+}
