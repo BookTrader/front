35070234aab7e682edd0718573067c523a11d9fd
rodrigogeraldes <rodrigo.geraldes@aluno.ifsp.edu.br>
2021-06-19 15:49:09 -0300

    Menu drawer navigator

--- App.js
+++ App.js
@@ -1,70 +1,108 @@
 import React from 'react';
+
 import Login from './src/telas/AcessoLivre/Login';
 import Register from './src/telas/AcessoLivre/Register/Register';
 import Feed from './src/telas/AcessoLivre/Feed/Feed';
 import CriarAnuncio from './src/telas/AcessoUsuario/Anuncio/CriarAnuncio';
+import { DrawerContent } from './src/telas/Navegacao/DrawerContent';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { Ionicons } from '@expo/vector-icons';
+
+
 import AuthProvider from './src/context/auth';
 
-const Stack = createStackNavigator();
+const FeedStack = createStackNavigator();
+const LoginStack = createStackNavigator();
+const CriarAnuncioStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
+const FeedStackScreen = ({navigation}) => (
+  <FeedStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor:  '#e53945',
+    },
+    headerTintColor: '#FFF',
+    headerTitleAlign: 'center',
+    headerTitleStyle:{
+      fontWeight:'bold'
+    }
+  }}>
+      <FeedStack.Screen  name="Feed" component={Feed} options={{
+          title: 'AlleyBook',
+          headerLeft: () => (
+            <Ionicons style={{paddingLeft: 10}} name="ios-menu" size={35}
+            backgroundColor="#e53945" color="#FFF" onPress={() => navigation.openDrawer()}></Ionicons>
+          )
+        }}
+      />
+</FeedStack.Navigator>
+)
 
-function DrawerRoutes() {
-  return (
-      <Drawer.Navigator>
-        <Drawer.Screen name="Feed" component={Feed} />
-        <Drawer.Screen name="Criar um anúncio" component={CriarAnuncio} />
-      </Drawer.Navigator>
-  );
-}
-//Get data
+const CriarAnuncioStackScreen = ({navigation}) => (
+  <CriarAnuncioStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor:  '#e53945',
+    },
+    headerTintColor: '#FFF',
+    headerTitleAlign: 'center',
+    headerTitleStyle:{
+      fontWeight:'bold'
+    }
+  }}>
+      <CriarAnuncioStack.Screen  name="CriarAnuncio" component={CriarAnuncio} options={{
+          title: 'Criar um anúncio',
+          headerLeft: () => (
+            <Ionicons style={{paddingLeft: 10}} name="ios-menu" size={35}
+            backgroundColor="#e53945" color="#FFF" onPress={() => navigation.openDrawer()}></Ionicons>
+          )
+        }}
+      />
+</CriarAnuncioStack.Navigator>
+)
+
+const LoginStackScreen = ({navigation}) => (
+  <LoginStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor:  '#e53945',
+    },
+    headerTintColor: '#FFF',
+    headerTitleAlign: 'center',
+    headerTitleStyle:{
+      fontWeight:'bold'
+    }
+  }}>
+      <LoginStack.Screen name="Login" component={Login} options={{
+        title: 'Entrar',
+        headerLeft: () => (
+          <Ionicons style={{paddingLeft: 10}} name="ios-menu" size={35}
+          backgroundColor="#e53945" color="#FFF" onPress={() => navigation.openDrawer()}></Ionicons>
+        )
+      }}
+      />
+
+      <LoginStack.Screen name="Register"  component={Register} options={{
+        title: 'Cadastrar'
+      }}
+      />
+</LoginStack.Navigator>
+)
 
 
 function App() {
     return (
       <AuthProvider>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login">
-              <Stack.Screen 
-              name="Login" 
-              component={Login}
-              options={{
-                title: 'Entrar',
-                headerStyle:{
-                  backgroundColor: '#e53945'
-                },
-                headerTintColor: '#FFF'
-              }}
-              />
-
-              <Stack.Screen 
-              name="Register" 
-              component={Register}
-              options={{
-                title: 'Cadastrar',
-                headerStyle:{
-                  backgroundColor: '#e53945'
-                },
-                headerTintColor: '#FFF'
-              }}
-              />
-
-              <Stack.Screen 
-                name="Feed" 
-                component={DrawerRoutes}
-                options={{
-                  title: 'Feed',
-                  headerStyle:{
-                  backgroundColor: '#e53945'
-                  },
-                  headerTintColor: '#FFF'
-                }}
-              />
-          </Stack.Navigator>
+        <Drawer.Navigator drawerContent={props => <DrawerContent { ... props} />} initialRouteName="Feed">
+        <Drawer.Screen name="Feed" component={FeedStackScreen} />
+        <Drawer.Screen name="Login" component={LoginStackScreen} />
+        <Drawer.Screen name="CriarAnuncio" component={CriarAnuncioStackScreen} />
+      </Drawer.Navigator>
+        {/* 
+         */}
         </NavigationContainer>
       </AuthProvider>
     );
---
+++ src/telas/Navegacao/DrawerContent.js
@@ -0,0 +1,145 @@
+import React from 'react';
+
+import { View, StyleSheet } from 'react-native';
+import { 
+    Avatar,
+    Title,
+    Caption,
+    Drawer,
+} from 'react-native-paper';
+
+import {
+    DrawerContentScrollView,
+    DrawerItem
+} from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export function DrawerContent(props){
+    return(
+        <View style={{flex:1}}>
+                <DrawerContentScrollView { ... props}>
+                    <View style={styles.drawerContent}>
+
+                        <View style={styles.userInfoSection}>
+                            <View style={{flexDirection: 'row', marginTop: 15}}>
+                                <Avatar.Image 
+                                    source={require('./assets/rodrigo-foto.jpg')}
+                                    size={50}
+                                />
+                                <View style={{flexDirection: 'column', marginLeft: 15}}>
+                                    <Title style={styles.title}>Rodrigo Geraldes</Title>
+                                    <Caption style={styles.caption}>rodrigo@gmail.com</Caption>
+                                </View>
+                            </View>
+                        </View>
+
+                        <Drawer.Section style={styles.drawerSection}>
+                                <DrawerItem icon={({size}) => (
+                                    <Icon 
+                                    name="home-outline"
+                                    color="#e53945"
+                                    size={size}
+                                    />
+                                )}
+                                label="Feed"
+                                onPress={() => {props.navigation.navigate('Feed')}}
+                            />
+
+                             <DrawerItem icon={({size}) => (
+                                    <Icon 
+                                    name="account-outline"
+                                    color="#e53945"
+                                    size={size}
+                                    />
+                                )}
+                                label="Acesse sua conta"
+                                onPress={() => {props.navigation.navigate('Login')}}
+                            />
+
+                            <DrawerItem icon={({size}) => (
+                                    <Icon 
+                                    name="bookmark-outline"
+                                    color="#e53945"
+                                    size={size}
+                                    />
+                                )}
+                                label="Criar um anúncio"
+                                onPress={() => {props.navigation.navigate('CriarAnuncio')}}
+                            />
+
+                            <DrawerItem icon={({size}) => (
+                                    <Icon 
+                                    name="account-check-outline"
+                                    color="#e53945"
+                                    size={size}
+                                    />
+                                )}
+                                label="Configurações"
+                                onPress={() => {}}
+                            />
+
+                        </Drawer.Section>
+
+                    </View>
+                </DrawerContentScrollView>
+                <Drawer.Section style={styles.bottomDrawerSection}>
+                    <DrawerItem icon={({size}) => (
+                            <Icon 
+                            name="exit-to-app"
+                            color="#e53945"
+                            size={size}
+                            />
+                        )}
+                        label="Sair"
+                        onPress={() => {}}
+                    />
+                </Drawer.Section>
+        </View>
+    );
+} 
+
+const styles = StyleSheet.create({
+    drawerContent:{
+        flex: 1,
+    },
+    userInfoSection: {
+        paddingLeft: 20,
+    },
+    title:{
+        fontSize: 16,
+        marginTop: 3,
+        fontWeight: 'bold',
+    },
+    caption: {
+        fontSize: 14,
+        lineHeight: 14,
+    },
+    row: {
+        marginTop: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    section: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginRight: 15,
+    },
+    paragraph: {
+        fontWeight: 'bold',
+        marginRight: 3,
+    },
+    drawerSection: {
+        marginTop: 15,
+    },
+    bottomDrawerSection: {
+        marginBottom: 15,
+        borderTopColor: '#f4f4f4',
+        borderTopWidth: 1
+    },
+    preference: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+    },
+});
