e868d2011ddfcad4d4807b8173e77938ad309b11
rodrigogeraldes <rodrigo.geraldes@aluno.ifsp.edu.br>
2021-11-04 23:29:19 -0300

    Criação de modal utilizando modalize

--- src/telas/AcessoUsuario/Perfil/EditarPerfil.js
+++ src/telas/AcessoUsuario/Perfil/EditarPerfil.js
@@ -1,265 +1,216 @@
-import React from 'react';
-import { 
-  KeyboardAvoidingView, 
-  View,
-  Text,
-  TouchableOpacity,
-  ImageBackground,
-  TextInput,
-  StyleSheet,
-} from 'react-native';
-
-import BottomSheet from 'reanimated-bottom-sheet';
-import Animated from 'react-native-reanimated';
+import React, {useRef} from 'react';
+import {
+    KeyboardAvoidingView,
+    View,
+    Text,
+    TouchableOpacity,
+    ImageBackground,
+    TextInput,
+    StyleSheet,
+} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-
-
+import { Modalize } from 'react-native-modalize';
 
 export default function Perfil({ navigation }) {
 
-  const bs = React.useRef();
-  const fall = new Animated.Value(1);
+    const modalizeRef = useRef(null);
+    
+    function onOpen(){
+        modalizeRef.current?.open();
+    }
 
-  
-  const renderInner = () => (
-    <View style={styles.panel}>
-      <View style={{alignItems: 'center'}}>
-        <Text style={styles.panelTitle}>Upload de Foto</Text>
-        <Text style={styles.panelSubtitle}>Selecione sua foto de perfil</Text>
-      </View>
-      <TouchableOpacity style={styles.panelButtom}>
-        <Text style={styles.panelButtonTitle}>Tirar foto</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.panelButtom}>
-        <Text style={styles.panelButtonTitle}>Escolher da galeria</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => bs.current.snapTo(1)}
-        style={styles.panelButtom}>
-        <Text style={styles.panelButtonTitle}>Cancelar</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    function onClose(){
+        modalizeRef.current?.close();
+    }
 
-  const renderHeader = () => (
-    <View style={styles.header}>
-      <View style={styles.panelHeader}>
-        <View style={styles.panelHandle}/>
-      </View>
-    </View>
-  );
-  
 
-  return (
-    <KeyboardAvoidingView style={styles.container} keyboardVerticalOffset={80}>
-      <BottomSheet ref={bs}
-                  snapPoints={[330, 0]}
-                  renderContent={renderInner}
-                  renderHeader={renderHeader}
-                  initialSnap={1}
-                  callbackNode={fall}
-                  enabledGestureInteraction={true} 
-                  />   
-          <Animated.View style={{margin: 20,
-          opacity: Animated.add(0.1, Animated.multiply(fall, 1.0)),
-          }}>  
-      <View style={{margin: 20}}>
-        <View style={{alignItems: 'center'}}>
-          <TouchableOpacity onPress={()=> bs.current.snapTo(0)}>
-            <View style={{
-              height: 100,
-              width: 100,
-              borderRadius: 15,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-              <ImageBackground source={require(
-        '../../../../assets/rodrigo-foto.jpg'
-        )} style={{
-          height: 100, width:100}}
-          imageStyle={{borderRadius: 15}}>
-                <View style={{
-                  flex: 1,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                  <Icon name="camera" size={35} color="#fff" style={{
-                    opacity: 0.7,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    borderWidth: 1,
-                    borderColor: '#fff',
-                    borderRadius: 10,
-                  }}/>
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            keyboardVerticalOffset={80}
+        >
+            <View style={{ margin: 20 }}>
+                <View style={{ alignItems: 'center' }}>
+                    <TouchableOpacity onPress={onOpen}>
+                        <View
+                            style={{
+                                height: 100,
+                                width: 100,
+                                borderRadius: 15,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <ImageBackground
+                                source={require('../../../../assets/rodrigo-foto.jpg')}
+                                style={{
+                                    height: 100,
+                                    width: 100,
+                                }}
+                                imageStyle={{ borderRadius: 15 }}
+                            >
+                                <View
+                                    style={{
+                                        flex: 1,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <Icon
+                                        name="camera"
+                                        size={35}
+                                        color="#fff"
+                                        style={{
+                                            opacity: 0.7,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            borderWidth: 1,
+                                            borderColor: '#fff',
+                                            borderRadius: 10,
+                                        }}
+                                    />
+                                </View>
+                            </ImageBackground>
+                        </View>
+                    </TouchableOpacity>
                 </View>
-              </ImageBackground>
             </View>
-          </TouchableOpacity>
-        </View> 
-      </View>
 
-      <View style={styles.form}>
-      <Text style={styles.label}>Dados Pessoais</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Apelido"
-            autoCorrect={false}
-          />
+            <View style={styles.form}>
+                <Text style={styles.label}>Dados Pessoais</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Apelido"
+                    autoCorrect={false}
+                />
 
-          <TextInput
-            style={styles.input}
-            placeholder="Nome"
-            autoCorrect={false}
-          />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome"
+                    autoCorrect={false}
+                />
 
-          <TextInput
-            style={styles.input}
-            placeholder="CPF"
-            autoCorrect={false}
-          />
+                <TextInput
+                    style={styles.input}
+                    placeholder="CPF"
+                    autoCorrect={false}
+                />
 
-          <TouchableOpacity 
-              style={styles.btnSubmit}
-          >
-              <Text style={styles.btnSubmitText}>
-                  Atualizar
-              </Text>
-          </TouchableOpacity>
-      </View>
-      </Animated.View>
-    </KeyboardAvoidingView>
-  );
-};
+                <TouchableOpacity style={styles.btnSubmit}>
+                    <Text style={styles.btnSubmitText}>Atualizar</Text>
+                </TouchableOpacity>
+            </View>
 
+            <Modalize
+                ref={modalizeRef}
+                snapPoint={220}
+                modalHeight={220}
+            >
+                <View style={styles.modalContent}>
+
+                    <TouchableOpacity style={styles.btnModal}>
+                        <Text style={styles.btnModalText}>Tirar foto</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.btnModal}>
+                        <Text style={styles.btnModalText}>Escolher da galeria</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={onClose} style={styles.btnModal}>
+                        <Text style={styles.btnModalText}>Cancelar</Text>
+                    </TouchableOpacity>
+
+                </View>
+            </Modalize>
+
+        </KeyboardAvoidingView>
+    )
+}
 
 const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: '#ECECEC',
-  },
-  form: {
-    alignSelf: 'stretch',
-    paddingHorizontal: 30,
-  },
-  label: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    color: '#031d44',
-    marginBottom: 8,
+    container: {
+        flex: 1,
+        backgroundColor: '#ECECEC',
     },
-  input: {
-      borderWidth: 1,
-      borderColor: '#031d44',
-      backgroundColor: '#FFF',
-      paddingHorizontal: 20,
-      fontSize: 16,
-      color: '#242424',
-      height: 44,
-      marginBottom: 10,
-      borderRadius: 5,
-  },
-  inputText: {
-      borderWidth: 1,
-      borderColor: '#031d44',
-      backgroundColor: '#FFF',
-      paddingHorizontal: 20,
-      fontSize: 16,
-      color: '#242424',
-      height: 84,
-      textAlignVertical : "top",
-      paddingTop: 10,
-      marginBottom: 10,
-      borderRadius: 5,
-  },
-  btnSubmit: {
-      backgroundColor: '#e53945',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingHorizontal: 20,
-      fontSize: 16,
-      color: '#242424',
-      height: 44,
-      marginBottom: 5,
-      borderRadius: 5,
-  },
-  btnSubmitText: {
-      color: '#FFF',
-      fontSize: 18,
-  },
-  commandButton: {
-    padding: 15,
-    borderRadius: 10,
-    backgroundColor: '#FF6347',
-    alignItems: 'center',
-    marginTop: 10,
-  },
-  panel: {
-    padding: 20,
-    backgroundColor: '#FFFFFF',
-    paddingTop: 20,
-  },
-  header: {
-    backgroundColor: '#FFFFFF',
-    shadowColor: '#333333',
-    shadowOffset: {width: -1, height: -3},
-    shadowRadius: 2,
-    shadowOpacity: 0.4,
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5,
-  },
-  panelHeader: {
-    paddingTop: 15,
-    alignItems: 'center',
-  },
-  panelHandle: {
-    width: 40,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: '#00000040',
-    marginBottom: 10,
-  },
-  panelTitle: {
-    fontSize: 27,
-    height: 35,
-  },
-  panelSubtitle: {
-    fontSize: 14,
-    color: 'gray',
-    height: 30,
-    marginBottom: 10,
-  },
-  panelButtom: {
+    modalHeader: {
+        alignItems: 'center',
+    },
+    modalContent: {
+        flex: 1,
+        height: 180,
+        marginTop: 30,
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+    },
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+    },
+    label: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#031d44',
+        marginBottom: 8,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#031d44',
+        backgroundColor: '#FFF',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#242424',
+        height: 44,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    inputText: {
+        borderWidth: 1,
+        borderColor: '#031d44',
+        backgroundColor: '#FFF',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#242424',
+        height: 84,
+        textAlignVertical: 'top',
+        paddingTop: 10,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    btnSubmit: {
+        backgroundColor: '#e53945',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#242424',
+        height: 44,
+        marginBottom: 5,
+        borderRadius: 5,
+    },
+    btnSubmitText: {
+        color: '#FFF',
+        fontSize: 18,
+    },
+    btnModalText: {
+        color: '#FFF',
+        fontSize: 18,
+    },
+    btnModal: {
+        backgroundColor: '#e53945',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#242424',
+        height: 44,
+        marginBottom: 5,
+        borderRadius: 5,
+        marginTop: 5,
+    },
     
-    padding: 13,
-    borderRadius: 5,
-    backgroundColor: '#e53945',
-    alignItems: 'center',
-    marginVertical: 7,
-  },
-  panelButtonTitle: {
-    fontSize: 17,
-    color: 'white',
-  },
-  action: {
-    flexDirection: 'row',
-    marginTop: 10,
-    marginBottom: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#f2f2f2',
-    paddingBottom: 5,
-  },
-  actionError: {
-    flexDirection: 'row',
-    marginTop: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#FF0000',
-    paddingBottom: 5,
-  },
-  textInput: {
-    flex: 1,
-    paddingLeft: 10,
-    color: '#05375a',
-  },
-
-  
-});
+    textInput: {
+        flex: 1,
+        paddingLeft: 10,
+        color: '#05375a',
+    },
+})
