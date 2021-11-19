e416db61e88c8eb25984138476f59b1c9e37998e
wesferoli <wesleyfer.oli97@gmail.com>
2021-11-19 14:04:32 -0300

    Pegando dados de localização do usuário

---
+++ src/context/location.js
@@ -0,0 +1,50 @@
+import React, { useState, useContext, createContext, useEffect } from "react";
+
+import * as Location from 'expo-location';
+import { Alert } from "react-native";
+
+import { api } from "../service/api";
+import { useAuth } from "./auth";
+
+const LocationContext = createContext();
+
+export default function LocationProvider({children}) {
+
+  const [location, setLocation] = useState();
+  const { usuario } = useAuth();
+
+  async function getLocation() {
+
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+
+    const [usr_latitude, usr_longitude] = [location.coords.latitude, location.coords.longitude];
+
+    if (usuario) {
+      await api.patch(`/usuario/${usuario.id}`, { usr_latitude, usr_longitude })
+    }
+
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, [usuario]);
+
+  return(
+    <LocationContext.Provider value={{location, setLocation, getLocation}}>
+        {children}
+    </LocationContext.Provider>
+  );
+}
+
+export function useLocation() {
+  const context = useContext(LocationContext);
+  
+  return context;
+}
